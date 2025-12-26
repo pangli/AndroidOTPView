81579e1503fa8eeacf3e313bd5928247186f8623
佐罗 <674736456@qq.com>
2025-12-26 16:45:08 +0800

    OTP View

---
+++ app/src/main/java/com/zorro/otpview/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.zorro.otpview
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.zorro.otpview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        vb.apply {
+            validateButton.setOnClickListener {
+                Toast.makeText(this@MainActivity, otpView1.text, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, otpView2.text, Toast.LENGTH_SHORT).show()
+            }
+            otpView1.setOtpCompletionListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "OnOtpCompletionListener called",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            otpView2.setOtpCompletionListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "OnOtpCompletionListener called",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
---
+++ optview/src/main/java/com/zorro/optview/OnOtpCompletionListener.java
@@ -0,0 +1,5 @@
+package com.zorro.optview;
+
+public interface OnOtpCompletionListener {
+  void onOtpCompleted(String otp);
+}
